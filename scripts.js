16a024762e184b4cc7aa6d0515c6710036cc60e7
Syed (Daniyal) Shahid <98437182+Daniyal-Shahid21@users.noreply.github.com>
2024-01-23 15:42:39 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function headerVisible() {
+    var headerElement = document.querySelector('header');
+    headerElement.style.backgroundColor = 'darkgreen';
+    headerElement.style.color = 'maroon';
+}
+
+function headerHidden() {
+    var headerElement = document.querySelector('header');
+    headerElement.style.backgroundColor = 'transparent';
+    headerElement.style.color = 'transparent';
+}
